559ea44f1eb5ecb72191a48aeb773eca58d03ea5
Peijing Li <peli@stanford.edu>
2026-06-27 17:45:24 -0700

    first astro migration

---
+++ src/data/profile.ts
@@ -0,0 +1,15 @@
+export const siteTitle = 'Peijing Li Portfolio';
+
+export const profile = {
+  name: 'Peijing Li',
+  avatar: '/images/profile.jpg',
+  bio: 'PhD Student in Electrical Engineering',
+  location: 'Stanford, CA',
+  employer: 'Stanford University',
+  links: {
+    googlescholar: 'https://scholar.google.com/citations?user=m96juKIAAAAJ',
+    orcid: 'https://orcid.org/0000-0001-8517-6109',
+    github: 'https://github.com/peijli',
+    linkedin: 'https://www.linkedin.com/in/peijing-li',
+  },
+};
